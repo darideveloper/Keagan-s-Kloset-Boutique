266eaff533f0f0e37b39c5c9f6123fe75ba8df28
daridev <hernandezdarifrancisco@gmail.com>
2022-05-13 05:26:03 -0500

    thanks notification

---
+++ code/register.js
@@ -0,0 +1,16 @@
+window.addEventListener('locationchange', show_alert)
+
+function show_alert () {
+    var url = window.location.href
+    console.log (url)
+    if (url.includes("#thanks")) {
+        
+        // Create alert
+        Swal.fire('Thank you for registering your email', 'You will receive promotions in your email soon')
+
+        // Redirect
+        window.location.href = "#header"
+    }
+}
+
+show_alert ()
