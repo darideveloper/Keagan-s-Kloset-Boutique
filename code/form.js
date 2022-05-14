4f26bfeb899f37a62b725f9468cae90739c245d5
daridev <hernandezdarifrancisco@gmail.com>
2022-05-14 15:07:39 -0500

    validate client inputs

--- code/form.js
+++ code/form.js
@@ -8,6 +8,7 @@ const input_email = document.querySelector ("#email")
 const input_pc = document.querySelector ("#pc")
 const input_address = document.querySelector ("#address")
 const input_comments = document.querySelector ("#comments")
+const input_submit = document.querySelector ("input.btn")
 
 const total_elem = document.querySelector ("p.total > span")
 
@@ -43,3 +44,61 @@ function update_total_price () {
     let total = parseFloat(product_price) * parseFloat(input_quantity.value) + " USD"
     total_elem.innerHTML = total
 }
+
+function activate_submit () {
+    // Activate submit button
+
+    // Check input values 
+    let inputs = [input_name, input_email, input_pc, input_address]
+    let valid_inputs = true
+    for (let input of inputs) {
+        value = input.value
+        if (!value) {
+            valid_inputs = false
+        }
+    }
+
+    // Activate submit button
+    if (valid_inputs) {
+        input_submit.classList.remove ("disabled")
+        input_submit.removeAttribute ("disabled")
+    } else {
+        input_submit.classList.add ("disabled")
+        input_submit.setAttribute ("disabled", "")
+    }
+}
+
+function validate_input (input_elem, min_lenght) {
+
+    // Validate input lenght
+    let input_text = input_elem.value
+    let input_error = input_elem.parentNode.querySelector ("span.error")
+    let message = "The " + input_elem.getAttribute ("name")+ " must have at least " + min_lenght + " characters"
+    if (input_text.length < min_lenght) {
+        input_error.classList.remove ("hide")
+        input_error.innerHTML = message
+    } else {
+        input_error.classList.add ("hide")
+    }
+
+}
+
+input_name.addEventListener ("change", function (e) {
+    activate_submit ()
+    validate_input (input_name, 3)
+})
+
+input_email.addEventListener ("change", function (e) {
+    activate_submit ()
+    validate_input (input_email, 8)
+})
+
+input_pc.addEventListener ("change", function (e) {
+    activate_submit ()
+    validate_input (input_pc, 5)
+})
+
+input_address.addEventListener ("change", function (e) {
+    activate_submit ()
+    validate_input (input_address, 10)
+})
