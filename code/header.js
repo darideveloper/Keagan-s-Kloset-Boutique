4b91c5e71e7d8ff7b59c8f4629a58964d64bafca
daridev <hernandezdarifrancisco@gmail.com>
2022-05-16 00:36:39 -0500

    hide menu when click link

--- code/header.js
+++ code/header.js
@@ -1,8 +1,16 @@
 const header_nav = document.querySelector ("#header nav")
 const header_btn = document.querySelector ("#header button")
+const header_links = document.querySelectorAll ("#header a")
 
 // Show and collapse menu
-header_btn.addEventListener ("click", function (e) {
+header_btn.addEventListener ("click", show_hide_menu)
+
+// Hide header when click a header link
+for (let header_link of header_links) {
+    header_link.addEventListener ("click", show_hide_menu)
+}
+
+function show_hide_menu () {
     header_nav.classList.toggle ("active")
     header_btn.classList.toggle ("active")
-})
+}
